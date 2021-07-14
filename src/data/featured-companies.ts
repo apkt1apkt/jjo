e15d6ad1a5993e09adaf413c2cc1a0f3d495e660
apkt1apkt <tsatsuarnold@gmail.com>
2021-07-14 23:36:31 +0000

    add dummy data

---
+++ src/data/featured-companies.ts
@@ -0,0 +1,11 @@
+export const FEATURED_COMPANIES = [
+  "GrandParade",
+  "Seargin",
+  "DEX Ventures",
+  "NBC",
+  "SVT SP Zoo",
+  "DG Tech",
+  "PickSaaS",
+  "BlackRose",
+  "FrontKom",
+].map((v) => ({ label: v, icon: "fa-briefcase" }));
---
+++ src/data/job-offers.ts
@@ -0,0 +1,25 @@
+export const JOB_OFFERS = [
+  {
+    title: "Junior Java Developer",
+    company: "PickSaaS",
+    location: "Wrclaw, Polska",
+    time: "2 hours ago",
+    description:
+      "I am currently looking or a Java Developer, Project for Public Sector Location: Wroclaw (during the pandemic remote work)\n\n$ up to PLN 550 net / MD B2B",
+  },
+  {
+    title: "Junior Project Manager",
+    company: "Frontkom",
+    location: "Lublin, Polska",
+    time: "3 hours ago",
+    description: `Join us in the Lublin office to build innovative long-term projects. Exciting international clients and partners and a work environment focused on learning and growth awaits you!`,
+  },
+  {
+    title: "Junior Project Manager",
+    company: "Fontkom",
+    location: "Lublin, Polksa",
+    time: "1 day ago",
+    description:
+      "I am currently looking for a Java Developer. Project for Public Sector Location: Wroclaw (during the pandemic remote work)\n$ up to PLN 550 net / MD B2B",
+  },
+];
