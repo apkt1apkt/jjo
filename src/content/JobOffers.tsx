226dea16d5cc6b1c27394183b09e14f1c9d5cecb
apkt1apkt <tsatsuarnold@gmail.com>
2021-07-15 00:37:51 +0000

    add 'view all' in job offers

--- src/content/JobOffers.tsx
+++ src/content/JobOffers.tsx
@@ -1,11 +1,18 @@
 import Header from "../components/Header";
 import JobOfferCard from "../components/JobOfferCard";
 import { JOB_OFFERS } from "../data/job-offers";
+import { primaryColor } from "../styles";
 
 export default function JobOffers() {
   return (
     <div style={STYLES.container}>
-      <Header heading="Newest Job Offers" />
+      <div style={STYLES.top}>
+        <Header heading="Newest Job Offers" />
+        <div style={STYLES.viewAll}>
+          View all job offers
+          <i style={STYLES.icon} className="fa fa-arrow-right" aria-hidden="true"></i>
+        </div>
+      </div>
       <div>
         <div style={STYLES.jobOffers}>
           {JOB_OFFERS.map((v, i) => (
@@ -25,5 +32,19 @@ const STYLES = {
     marginTop: 20,
     display: "grid",
     gridTemplateColumns: "33.3% 33.40% 33.3%",
+  },
+  top: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  viewAll: {
+    color: primaryColor,
+    fontSize: 10,
+    fontWeight: 800,
+    cursor: "pointer",
+  },
+  icon: {
+    paddingLeft: 3,
   },
 };
